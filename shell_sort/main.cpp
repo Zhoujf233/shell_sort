760e256b902deabdd81698b80785057913971b3c
zhoujf <zhoujf1636@163.com>
2019-08-19 10:52:21 +0800

    666

--- shell_sort/main.cpp
+++ shell_sort/main.cpp
@@ -7,9 +7,45 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+void shell_sort(int* arr, int size)
+{
+    int gap,i,j;
+    int temp;
+    for(gap = size >> 1; gap > 0; gap = gap >> 1)
+    {
+        for(i = gap; i < size; i++)
+        {
+            temp = arr[i];
+            for(j = i - gap; j >= 0 && arr[j] > temp; j -= gap)
+            {
+                arr[j + gap] = arr[j];
+            }
+            arr[j + gap] = temp;
+        }
+    }
+}
+
+int main(int argc, const char * argv[])
+{
+    int arr[] = {89,22,3,32,82,55,22,50,37,5,64,35,9,70};
+    int size = (int) sizeof(arr)/sizeof(*arr);
+    int i;
+    cout<<"初始顺序为: ";
+    for(i = 0;i < size;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    
+    shell_sort(arr, size);
+    
+    cout << "选择排序后顺序为：";
+    for(i = 0;i < size;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
     return 0;
 }
