d90aa51148ec26dea0a469d5d5992288b7a24a28
zhoujf <zhoujf1636@163.com>
2019-08-19 11:32:41 +0800

    hhh

--- shell_sort/main.cpp
+++ shell_sort/main.cpp
@@ -27,26 +27,73 @@ using namespace std;
 //    }
 //}
 
+//希尔排序
+//void shell_sort(int* arr, int size)
+//{
+//    int i,j,gap;
+//    for(gap = size / 2; gap > 0; gap /= 2)//步长
+//    {
+//        for(i = 0; i < gap; i++)//直接插入排序
+//        {
+//            for(j = i + gap; j < size; j += gap)
+//            {
+//                if(arr[j] < arr[j - gap])
+//                {
+//                    int temp = arr[j];
+//                    int k = j - gap;
+//                    while(k >= 0 && arr[k] > temp)
+//                    {
+//                        arr[k + gap] = arr[k];
+//                        k -= gap;
+//                    }
+//                    arr[k + gap] = temp;
+//                }
+//            }
+//        }
+//    }
+//}
+
+//优化1
+//void shell_sort(int* arr, int size)
+//{
+//    int j,gap;
+//    for(gap = size / 2; gap > 0; gap /= 2)
+//    {
+//        for(j = gap; j < size; j++)//从数组第gap个元素开始
+//        {
+//            if(arr[j] < arr[j - gap])//每个元素与自己组内的数据进行直接插入排序
+//            {
+//                int temp = arr[j];
+//                int k = j - gap;
+//                while(k >= 0 && arr[k] > temp)
+//                {
+//                    arr[k + gap] = arr[k];
+//                    k -= gap;
+//                }
+//                arr[k + gap] = temp;
+//            }
+//        }
+//    }
+//}
+
+//优化2
+void swap(int *a, int *b)//交换两数
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
 void shell_sort(int* arr, int size)
 {
     int i,j,gap;
-    for(gap = size / 2; gap > 0; gap /= 2)//步长
+    for(gap = size / 2; gap > 0; gap /= 2)
     {
-        for(i = 0; i < gap; i++)//直接插入排序
+        for(i = gap; i < size; i++)
         {
-            for(j = i + gap; j < size; j += gap)
+            for(j = i - gap; j >= 0 && arr[j] > arr[j + gap]; j -= gap)
             {
-                if(arr[j] < arr[j - gap])
-                {
-                    int temp = arr[j];
-                    int k = j - gap;
-                    while(k >= 0 && arr[k] > temp)
-                    {
-                        arr[k + gap] = arr[k];
-                        k -= gap;
-                    }
-                    arr[k + gap] = temp;
-                }
+                swap(arr[j], arr[j + gap]);
             }
         }
     }
